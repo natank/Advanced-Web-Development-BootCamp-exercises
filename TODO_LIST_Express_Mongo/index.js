4b4de0228add14ec846a163becbdab68f20ff8e6
Nati Kaumusher <natan.kamuhser@gmail.com>
2018-09-01 20:38:03 +0300

    removing unnecessary routes

--- TODO_LIST_Express_Mongo/index.js
+++ TODO_LIST_Express_Mongo/index.js
@@ -7,16 +7,7 @@ process.env.PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', function(req, res){
-	res.json({message: "HI FROM THE ROOT ROUTE"})
-});
-
 app.use('/api/todos', todoRoutes);
-
-app.get('/happy', function(req, res){
-	res.send(":-)")
-})
 
 app.listen(process.env.PORT, function(){
 	console.log("App is running on port: "+ process.env.PORT);
